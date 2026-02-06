8ac87c1ee57d2ec15a21915af4ed1461aad62e00
Seth Taylor <taylorseth59@gmail.com>
2026-02-05 22:23:57 -0600

    set up cpmonate structure and basic layout

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+        <span className="header__span">
+      <h1 className='header__tittle'>Rabbit Planner</h1>
+      </span>
+      <button className="header__btn">Add Task</button>
+    </header>
+  )
+}
+
+export default Header;
